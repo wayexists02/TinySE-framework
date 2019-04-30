79d2ee62c473428a0d80622a963eb4be28a1cb2c
Younghoon Kim <nongaussian@gmail.com>
2019-04-30 17:11:21 +0900

    stage 2: test program

--- src/main/java/edu/hanyang/perfeval/EvalExternalSort.java
+++ src/main/java/edu/hanyang/perfeval/EvalExternalSort.java
@@ -5,23 +5,28 @@ import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import edu.hanyang.submit.TinySEExternalSort;
+import edu.hanyang.indexer.ExternalSort;
 
 public class EvalExternalSort {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 		String infile = "./test-10000000.data";
 		String outfile = "./sort-10000000.data";
 		String tmpdir = "./tmp";
 		int blocksize = 4096;
 		int nblocks = 100;
 
-		TinySEExternalSort sort = new TinySEExternalSort();
+		Class<?> cls = Class.forName("edu.hanyang.submit.TinySEExternalSort");
+		ExternalSort sort = (ExternalSort) cls.newInstance();
+		
+		//TinySEExternalSort sort = new TinySEExternalSort();
 		long s = System.currentTimeMillis();
 		sort.sort(infile, outfile, tmpdir, blocksize, nblocks);
 		long e = System.currentTimeMillis();
 
 		String answerfile = "./result-10000000.data";
+		@SuppressWarnings("resource")
 		DataInputStream answerdis = new DataInputStream(new BufferedInputStream(new FileInputStream(answerfile)));
+		@SuppressWarnings("resource")
 		DataInputStream datadis = new DataInputStream(new BufferedInputStream(new FileInputStream(outfile)));
 
 		for (int i = 0; i < 10000000 * 3; i++) {
