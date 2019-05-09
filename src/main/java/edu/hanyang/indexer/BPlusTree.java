70670d2e6c8421878437da4aa391413c9aecf60f
Younghoon Kim <nongaussian@gmail.com>
2019-05-09 10:02:16 +0900

    BPlusTree interface updated

--- src/main/java/edu/hanyang/indexer/BPlusTree.java
+++ src/main/java/edu/hanyang/indexer/BPlusTree.java
@@ -1,8 +1,10 @@
 package edu.hanyang.indexer;
 
+import java.io.IOException;
+
 public interface BPlusTree {
-	public void open(String metafile, String filepath, int blocksize, int nblocks);
-	public void insert(int key, int val);
-	public int search(int key);
-	public void close();
+	public void open(String metafile, String filepath, int blocksize, int nblocks)  throws IOException;
+	public void insert(int key, int val)  throws IOException;
+	public int search(int key)  throws IOException;
+	public void close() throws IOException;
 }
